31e4aac706ac129a02992b6f316b81d08cfece91
Emma <emma.cp128@gmail.com>
2018-11-19 13:01:28 +0100

    dit is het unity project

---
+++ Assets/GrowOverTime.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class GrowOverTime : MonoBehaviour {
+    [Tooltip("The maximum random scale factor per frame.")]
+    [Range(0.1f, 3)]
+    public float maxScalePerFrame = 0.5f;
+	
+	void Update () {
+        float randomScale = Random.Range(0.1f, maxScalePerFrame);
+        transform.localScale += new Vector3(Time.deltaTime * randomScale,
+                                            Time.deltaTime * randomScale,
+                                            Time.deltaTime * randomScale);
+    }
+}
